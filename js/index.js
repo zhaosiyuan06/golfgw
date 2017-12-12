4d5424dcecc4b6bad01f88024a3e8525958c42ce
zhaosiyuan06 <15735800437@163.com>
2017-12-12 17:44:56 +0800

    订单确认

--- js/index.js
+++ js/index.js
@@ -1,60 +1,86 @@
-$(function(){
+$(function () {
     //视屏播放
-    $(".backgroundImage").on("click",function(){
-        var imgSrc=$(this).attr("src-data")
-        $(".video_zhezhao .videoplay_Box video").attr("src",imgSrc);
+    $(".backgroundImage").on("click", function () {
+        var imgSrc = $(this).attr("src-data")
+        $(".video_zhezhao .videoplay_Box video").attr("src", imgSrc);
         $(".video_zhezhao .videoplay_Box video")[0].play();
-        $(".video_zhezhao").css("display","block")
+        $(".video_zhezhao").css("display", "block")
     })
-    $(".video_zhezhao img").on("click",function(){
+    $(".video_zhezhao img").on("click", function () {
         $(".video_zhezhao .videoplay_Box video")[0].pause();
-        $(".video_zhezhao").css("display","none")
+        $(".video_zhezhao").css("display", "none")
     })
     //相关资讯鼠标经过样式
-    $(".golf_news .col-md-2  .news_box").on("mouseenter",function(){
-        $(this).css("margin-top","-20px")
+    $(".golf_news .col-md-2  .news_box").on("mouseenter", function () {
+        $(this).css("margin-top", "-20px")
     })
-    $(".golf_news .col-md-2  .news_box").on("mouseleave",function(){
-        $(this).css("margin-top","0px")
+    $(".golf_news .col-md-2  .news_box").on("mouseleave", function () {
+        $(this).css("margin-top", "0px")
     })
     //注册
-    $("#loginin").on("click",function(){
-        $(".register").css("display","block")
+    $("#loginin").on("click", function () {
+        $(".register").css("display", "block")
         $(".register .form-horizontal").addClass("down")
     })
-    $(".register .cancel").on("click",function(){
-        $(".register").css("display","none")
+    $(".register .cancel").on("click", function () {
+        $(".register").css("display", "none")
         $(".register .form-horizontal").removeClass("down")
     })
-   //登录
-    $(".login_register").on("click",function(){
-        $(".register").css("display","none")
-        $(".login").css("display","block")
+    //登录
+    $(".login_register").on("click", function () {
+        $(".register").css("display", "none")
+        $(".login").css("display", "block")
         $(".register .form-horizontal").removeClass("down")
         $(".login .form-horizontal").addClass("down")
     })
-    $(".login .cancel").on("click",function(){
-        $(".login").css("display","none")
+    $(".login .cancel").on("click", function () {
+        $(".login").css("display", "none")
         $(".login .form-horizontal").removeClass("down")
     })
 //    点赞
-    $(".content .container .new").on("click",function(){
-       if($(this).hasClass("news")){
-           $(this).removeClass("news")
-       }else{
-           $(this).addClass("news")
-       }
+    $(".content .container .new").on("click", function () {
+        if ($(this).hasClass("news")) {
+            $(this).removeClass("news")
+        } else {
+            $(this).addClass("news")
+        }
     })
 
-//   产品数量加减
-    $(".num1").on("click",function(){
-        var sum = $(".num").attr("value")
-        sum = sum-1;
-        $(".num").attr("value",sum)
+    // 产品数量加减
+    $(".num1").on("click", function () {
+        var n = $(this).next().val();
+        var num = parseInt(n) - 1;
+        if (num == 0) {
+            return
+        }
+        $(this).next().val(num);
     })
-    $(".num2").on("click",function(){
-        var sum1 = $(".num").attr("value")
-        sum1 = sum1+1;
-        $(".num").attr("value",sum1)
+    $(".num2").on("click", function () {
+        var n = $(this).prev().val();
+        var num = parseInt(n) + 1;
+        if (num == 0) {
+            return;
+        }
+        $(this).prev().val(num);
     })
+
+    //订单确认
+    function address() {
+        var parer = $(".content_address .container .row .col-md-4 .addressed p").html()
+        var people = $(".content_address .container .row .col-md-4 .addressed .man").html()
+        var tel = $(".content_address .container .row .col-md-4 .addressed .tel").html()
+        $(".submit_box .container .col-md-12 p").html(parer)
+    }
+    address()
+
+    //默认地址选定
+    $(".content_address .col-md-4").on("click", function () {
+        $(".content_address .col-md-4 .box_order").removeClass("addressed")
+        $(this).children().addClass("addressed")
+        address()
+    })
+
+
 })
+
+
